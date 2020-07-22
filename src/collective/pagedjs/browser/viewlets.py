dd0a5e1e26dd854877c1b824e7ec75d7dca237c5
Andreas Jung <info@zopyx.com>
2020-07-22 18:08:36 +0200

    moved styles to external file

--- src/collective/pagedjs/browser/viewlets.py
+++ src/collective/pagedjs/browser/viewlets.py
@@ -1,4 +1,6 @@
+import os
 import plone.api
+from Products.Five.browser import BrowserView
 from plone.app.layout.viewlets import common as base
 
 
@@ -6,3 +8,15 @@ class PagedJS(base.ViewletBase):
 
     def portal_url(self):
         return plone.api.portal.get().absolute_url()
+
+class PagedJSView(BrowserView):
+
+    def styles(self):
+        dirname = os.path.dirname(__file__)
+        fn = os.path.join(dirname, 'static', 'styles.css')
+        with open(fn, 'rb') as fp:
+            css = fp.read()
+        self.request.response.setStatus(200)
+        self.request.response.setHeader("content-type", "text/css")
+        self.request.response.setHeader("content-length", str(len(css)))
+        self.request.response.write(css)
